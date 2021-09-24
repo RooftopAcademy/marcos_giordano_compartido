056d65bbd4fe5700bf5b911f8298dfdeb9a478fe
Marcosl14 <marcos_l14@hotmail.com>
2021-09-24 07:27:09 -0300

    Create tsconfig.json file and removed all the .js files

--- scripts/product-list.js
+++
@@ -1,8 +0,0 @@
-import store from "./app.js";
-import productComponent from "../src/components/product.js";
-
-let productContainer = document.getElementById("product-container");
-
-store.showCatalog().forEach((product) => {
-	productContainer.innerHTML += productComponent(product);
-});
