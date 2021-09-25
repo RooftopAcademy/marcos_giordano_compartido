4a471fe039806968f6c8a1233bf9371f8b46138e
Marcos Giordano <marcos_l14@hotmail.com>
2021-09-25 10:54:33 -0300

    Remove unnecesary js files

--- src/components/asideNavBar.js
+++
@@ -1,3 +0,0 @@
-export default function asideNavBar(store) {
-    return "\n    <div class=\"navigation-links-desktop\">\n        <a class=\"button-link\" href=\"./productList.html\"\n            >PRODUCTOS</a\n        >\n        <a class=\"button-link\" href=\"./sign-up.html\">REGISTRARSE</a>\n        <div class=\"home-content-aside-login\">\n            <a class=\"button-link js-user\" href=\"#\"><i class=\"fas fa-user\"></i> &nbsp " + (store.user ? store.user.firstName.toUpperCase() : "Invitado") + "</a>\n        </div>\n    </div>\n    ";
-}
--- src/components/cartItem.js
+++
@@ -1,3 +0,0 @@
-export default function cartItem(item) {
-    return "\n    <tr>\n        <td>" + item.item.name + "</td>\n        <td>" + item.item.price + "</td>\n        <td>" + item.amount + "</td>\n        <td>" + item.amount * item.item.price + "</td>\n    </tr>\n";
-}
