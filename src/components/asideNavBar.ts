81d2af684b493116be027321bd1b65bbe9649913
Marcos Giordano <marcos_l14@hotmail.com>
2021-09-26 20:05:24 -0300

    Modify aside navbar to change user name

--- src/components/asideNavBar.ts
+++ src/components/asideNavBar.ts
@@ -2,15 +2,18 @@ import Store from "../Store";
 
 export default function asideNavBar(store: Store): string {
 	return `
-    <div class="navigation-links-desktop">
+    <div class="navigation-links-desktop" id="nav-bar-container">
         <a class="button-link" href="./productList.html"
             >PRODUCTOS</a
         >
         <a class="button-link" href="./signUp.html">REGISTRARSE</a>
         <div class="home-content-aside-login">
-            <a class="button-link js-user" href="./user.html"><i class="fas fa-user"></i> &nbsp ${
-							store.user ? store.user.firstName.toUpperCase() : "Invitado"
-						}</a>
+            <a class="button-link js-user" href="./user.html">
+                <i class="fas fa-user"></i> 
+                &nbsp ${
+									store.user ? store.user.firstName.toUpperCase() : "Invitado"
+								}
+            </a>
         </div>
     </div>
     `;
