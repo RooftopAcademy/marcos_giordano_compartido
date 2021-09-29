2f84bc67999fc08e25d4c8e3b27c001ea700f7dc
Marcos Giordano <marcos_l14@hotmail.com>
2021-09-29 12:13:38 -0300

    New burger button component

---
+++ src/components/burgerButton.ts
@@ -0,0 +1,25 @@
+export default function burgerButtonComponent(
+  burgerButton: HTMLElement,
+  footer: HTMLElement,
+  asideNavBarContainer: HTMLElement,
+  mainContent: HTMLElement
+) {
+  console.log(burgerButton);
+  burgerButton.addEventListener("click", burgerButtonClickEvent);
+
+  function burgerButtonClickEvent() {
+    if (asideNavBarContainer.style.display === "flex") {
+      asideNavBarContainer.style.display = "none";
+      asideNavBarContainer.classList.remove("float");
+      mainContent.classList.remove("disabled");
+      footer.classList.remove("disabled");
+    } else {
+      asideNavBarContainer.style.display = "flex";
+      asideNavBarContainer.classList.add("float");
+      mainContent.classList.add("disabled");
+      footer.classList.add("disabled");
+    }
+  }
+
+  return burgerButton;
+}
