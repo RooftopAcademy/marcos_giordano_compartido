3cea67afb09cfcb5b80868f02c5fca44c38374cf
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-01 17:40:58 -0300

    Move files to other folders

--- src/helpers/createNewProductButton.ts
+++
@@ -1,7 +0,0 @@
-export default function createNewProductButton(anchor: HTMLAnchorElement) {
-  anchor.innerHTML = "NUEVO PRODUCTO";
-  anchor.classList.add("button-link");
-  anchor.href = "./newProduct.html";
-  anchor.id = "product-creation-link";
-  return anchor;
-}
