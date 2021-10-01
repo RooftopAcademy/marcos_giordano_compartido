a6f742312051760594feed9602f2f02c027b00ef
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-01 17:38:39 -0300

    Change hrefs with hashes

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -1,10 +1,10 @@
 export default function footerRendering(): string {
-	return `
+  return `
         <div class="footer" id="footer">
             <p class="footer-text">Copyright © 2021 - Made by Marcos</p>
             <div class="footer-links">
-                <a href="#">Informacion Legal</a> |
-                <a href="#">Arrepentimiento de Compra</a>
+                <a href="#/">Informacion Legal</a> |
+                <a href="#/">Arrepentimiento de Compra</a>
             </div>
         </div>
 `;
--- src/components/productCarrousel.ts
+++ src/components/productCarrousel.ts
@@ -1,10 +1,8 @@
-import Product from "../classes/Product";
+import Product from "../entities/Product";
 
 export default function productCarrousel(product: Product): string {
   return `
-    <a href="./productDetails.html?id=${
-      product.id
-    }" class="home-content-offers-product">
+    <a href="#/productDetails?id=${product.id}" class="content-offers-product">
         <img src="${product.thumbnail}" alt="${product.name}" />
         <p>${product.name}</p>
         <h2>$${(product.price * (1 - product.discount / 100)).toFixed(0)}</h2>
