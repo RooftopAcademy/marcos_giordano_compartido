185a477744d03d3202237c7c4801e48a8fbe80d0
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-01 17:36:46 -0300

    Burger button disabling when changing routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
-import Store from "./classes/Store";
-import commonComponentsRendering from "./views-logic/commonComponentsRendering";
-import Router from "./routes/routes";
+import Store from "./entities/Store";
+import commonComponentsRendering from "./viewsLogic/commonComponentsRendering";
+import router from "./routes/router";
 
 //Instanciate the store
 let store: Store = new Store();
@@ -9,8 +9,20 @@ store.cart.load();
 store.loadUser();
 
 //common components rendering
-let mainContent: HTMLElement = document.querySelector("#main-content")!;
+let mainContent: HTMLElement = document.getElementById("main-content")!;
 commonComponentsRendering(store, mainContent);
 
 //Views rendering through paths
-Router(store, mainContent);
+
+router("", store, mainContent);
+
+window.addEventListener("hashchange", function () {
+  let footer: HTMLElement = document.getElementById("js-footer")!;
+  let asideNavBarContainer: HTMLElement = document.getElementById("side-bar")!;
+  asideNavBarContainer.style.display = "none";
+  asideNavBarContainer.classList.remove("float");
+  mainContent.classList.remove("disabled");
+  footer.classList.remove("disabled");
+
+  router(this.location.hash, store, mainContent);
+});
