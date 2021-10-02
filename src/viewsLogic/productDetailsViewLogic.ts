f1edce375832d1e11dfe51156d81b84b26c22afe
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-02 18:15:47 -0300

    Create independen function for every action in the view

--- src/viewsLogic/productDetailsViewLogic.ts
+++ src/viewsLogic/productDetailsViewLogic.ts
@@ -11,56 +11,85 @@ export default function ProductDetailsViewLogic(
   store: Store,
   mainContent: HTMLElement
 ) {
-  //get product Id from params
-  let productID: Array<string> = window.location.href.split("?")[1].split("=");
-  let product: Array<Product> = store.getProductById(productID[1]);
+  let product: Product = getProduct();
+  viewRender(product);
+  getProductComments(); //should bringh comments by ProductId from the API
+  buyProductEvents(product);
+  removeProductEvents(product.id);
 
-  //rendering the view
-  mainContent.innerHTML = productDetailsView(product[0]);
+  /*
+  Functions
+  */
 
-  //get the comments
-  let productCommentsContainer: HTMLElement = document.getElementById(
-    "product-comments-container"
-  )!;
-  getComments(productCommentsContainer);
+  //view rendering
 
-  //add events to buy buttons
-  let buyButtons: NodeListOf<HTMLElement> =
-    document.getElementsByName("buy-button")!;
-
-  buyButtons.forEach((buyButton: HTMLElement) => {
-    buyButton.addEventListener("click", function () {
-      store.cart.add(product[0]);
-      let productQuantity: HTMLElement =
-        document.getElementById("cart-quantity")!;
-      productQuantity.innerHTML = ` &nbsp ${store.cart.showAll().length}`;
-    });
-
-    if (store.user.privilege == PrivilegeEnum.admin) {
-      buyButton.after(createRemoveButton());
-    }
-  });
-
-  //add events to remove buttons
-  let removeButtons: NodeListOf<HTMLElement> =
-    document.getElementsByName("remove-button")!;
-
-  if (removeButtons != null) {
-    removeButtons.forEach((button: HTMLElement) => {
-      button.addEventListener("click", function () {
-        displayInfoContainer(
-          "¿Está seguro que desea eliminar éste producto?.",
-          true,
-          removeItem
-        );
-      });
-    });
+  function viewRender(product: Product): void {
+    mainContent.innerHTML = productDetailsView(product);
   }
 
-  function removeItem() {
-    store.removeProduct(productID[1]);
-    // return home and show info container success.
-    returnHome();
-    displayInfoContainer("El producto ha sido eliminado correctamente.");
+  //get product Id
+
+  function getProduct(): Product {
+    let productID: Array<string> = window.location.href
+      .split("?")[1]
+      .split("=");
+    let product: Array<Product> = store.getProductById(productID[1]);
+    return product[0];
+  }
+
+  //get product comments
+
+  function getProductComments(): void {
+    let productCommentsContainer: HTMLElement = document.getElementById(
+      "product-comments-container"
+    )!;
+    getComments(productCommentsContainer);
+  }
+
+  //buy button event
+
+  function buyProductEvents(product: Product): void {
+    let buyButtons: NodeListOf<HTMLElement> =
+      document.getElementsByName("buy-button")!;
+
+    buyButtons.forEach((buyButton: HTMLElement) => {
+      buyButton.addEventListener("click", function () {
+        store.cart.add(product);
+        increaseProductsQuantityInCart();
+      });
+
+      if (store.user.privilege == PrivilegeEnum.admin) {
+        buyButton.after(createRemoveButton());
+      }
+    });
+  }
+  function increaseProductsQuantityInCart(): void {
+    let productQuantity: HTMLElement =
+      document.getElementById("cart-quantity")!;
+    productQuantity.innerHTML = ` &nbsp ${store.cart.showAll().length}`;
+  }
+
+  //remove button event
+
+  function removeProductEvents(product: string): void {
+    let removeButtons: NodeListOf<HTMLElement> =
+      document.getElementsByName("remove-button")!;
+
+    if (removeButtons != null) {
+      removeButtons.forEach((button: HTMLElement) => {
+        button.addEventListener("click", function () {
+          displayInfoContainer(
+            "¿Está seguro que desea eliminar éste producto?.",
+            true,
+            removeItem
+          );
+        });
+      });
+    }
+    function removeItem(): void {
+      store.removeProduct(product);
+      returnHome();
+      displayInfoContainer("El producto ha sido eliminado correctamente.");
+    }
   }
 }
