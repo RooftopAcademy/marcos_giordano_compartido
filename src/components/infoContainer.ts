1f9e4dc8564ca23fa68ff12ec4560a626d33c81d
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-03 07:21:38 -0300

    Refactor info container

--- src/components/infoContainer.ts
+++ src/components/infoContainer.ts
@@ -7,52 +7,87 @@ export default function displayInfoContainer(
     "header, main, footer"
   )!;
   let infoContainer: HTMLElement = document.getElementById("info-container")!;
-  let paragraph: HTMLParagraphElement = document.getElementById(
-    "info-container-paragraph"
-  ) as HTMLParagraphElement;
 
+  disableDomElements(domElements);
+  showInfoContainer(infoContainer, text);
+  let infoContainerOKButton = addOkButton();
+  removeTrailingCancelButton(infoContainer);
+  addCancelButton(infoContainerOKButton);
+
+  /*
+  Buttons Functions
+  */
+
+  function addOkButton(): HTMLButtonElement {
+    let infoContainerOKButton: HTMLButtonElement = document.getElementById(
+      "info-container-ok-button"
+    ) as HTMLButtonElement;
+
+    infoContainerOKButton.addEventListener("click", executeFunction);
+
+    return infoContainerOKButton;
+  }
+
+  function addCancelButton(infoContainerOKButton: HTMLButtonElement): void {
+    if (showCancelButton) {
+      let cancelButton = createCancelButton();
+      infoContainerOKButton.after(cancelButton);
+
+      cancelButton.addEventListener("click", function () {
+        enableDomElements(domElements);
+        hideInfoContainer(infoContainer);
+        infoContainerOKButton.removeEventListener("click", executeFunction);
+      });
+    }
+  }
+
+  function executeFunction(): void {
+    enableDomElements(domElements);
+    hideInfoContainer(infoContainer);
+    if (performFunction && showCancelButton) {
+      performFunction();
+      infoContainerOKButton.removeEventListener("click", executeFunction);
+    }
+  }
+}
+
+function disableDomElements(domElements: NodeListOf<HTMLElement>): void {
   domElements.forEach((element: HTMLElement) => {
     element.style.opacity = "0.4";
     element.style.pointerEvents = "none";
   });
+}
+
+function enableDomElements(domElements: NodeListOf<HTMLElement>): void {
+  domElements.forEach((element: HTMLElement) => {
+    element.removeAttribute("style");
+  });
+}
+
+function showInfoContainer(infoContainer: HTMLElement, text: string): void {
   infoContainer.classList.add("display-info-container");
+  let paragraph: HTMLParagraphElement = document.getElementById(
+    "info-container-paragraph"
+  ) as HTMLParagraphElement;
   paragraph.innerText = text;
+}
 
-  let infoContainerOKButton: HTMLButtonElement = document.getElementById(
-    "info-container-ok-button"
-  ) as HTMLButtonElement;
+function hideInfoContainer(infoContainer: HTMLElement): void {
+  infoContainer.classList.remove("display-info-container");
+}
 
-  let exists = document.getElementById("info-container-cancel-button");
-  if (exists) {
-    infoContainer.removeChild(exists);
+function removeTrailingCancelButton(infoContainer: HTMLElement): void {
+  let existsCancelButton = document.getElementById(
+    "info-container-cancel-button"
+  );
+  if (existsCancelButton) {
+    infoContainer.removeChild(existsCancelButton);
   }
+}
 
-  if (showCancelButton) {
-    let cancelButton = document.createElement("button");
-    cancelButton.innerHTML = "Cancelar";
-    cancelButton.id = "info-container-cancel-button";
-    infoContainerOKButton.after(cancelButton);
-
-    cancelButton.addEventListener("click", function () {
-      domElements.forEach((element: HTMLElement) => {
-        element.removeAttribute("style");
-      });
-      infoContainer.classList.remove("display-info-container");
-    });
-  }
-
-  if (infoContainerOKButton) {
-    infoContainerOKButton.addEventListener("click", pepe);
-  }
-
-  function pepe() {
-    domElements.forEach((element: HTMLElement) => {
-      element.removeAttribute("style");
-    });
-    infoContainer.classList.remove("display-info-container");
-    if (performFunction && showCancelButton) {
-      performFunction();
-      infoContainerOKButton.removeEventListener("click", pepe);
-    }
-  }
+function createCancelButton(): HTMLButtonElement {
+  let cancelButton = document.createElement("button");
+  cancelButton.innerHTML = "Cancelar";
+  cancelButton.id = "info-container-cancel-button";
+  return cancelButton;
 }
