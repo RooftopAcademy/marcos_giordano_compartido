baabf5cdec44581977cd88ac7964e9719d34baa7
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-03 19:08:40 -0300

    Create search view

---
+++ src/views/searchView.ts
@@ -0,0 +1,10 @@
+export default function searchView() {
+  return `
+  <div class="search-bar">
+    <input type="text" />
+    <a class="navigation-button">
+      <i class="fas fa-search"></i>
+    </a>
+  </div>
+  `;
+}
