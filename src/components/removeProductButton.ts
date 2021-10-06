7f4248b967ff7edb03a1711e3ed711ae3e603668
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-06 17:56:31 -0300

    Rename the component

--- src/components/removeProductButton.ts
+++ src/components/removeProductButton.ts
@@ -1,7 +1,7 @@
-export default function createRemoveButton() {
+export default function createRemoveProductButton() {
   const removeButton: HTMLButtonElement = document.createElement("button");
   removeButton.innerHTML = "Eliminar";
   removeButton.classList.add("button-link");
-  removeButton.name = "remove-button";
+  removeButton.name = "remove-product-button";
   return removeButton;
 }
