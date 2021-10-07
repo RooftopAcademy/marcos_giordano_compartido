cc6d5c635eff70b6f1019ea2a14aaf193e0c7d59
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-07 09:29:59 -0300

    Add view form events

--- src/viewsLogic/editProductViewLogic.ts
+++ src/viewsLogic/editProductViewLogic.ts
@@ -4,11 +4,14 @@ import Store from "../entities/Store";
 import editProductView from "../views/editProductView";
 import NullProduct from "../entities/NullProduct";
 import returnErrorView from "../helpers/returnErrorView";
+import displayInfoContainer from "../components/infoContainer";
+import returnHome from "../helpers/returnHome";
 
 export function editProductViewLogic(store: Store, mainContent: HTMLElement) {
   const product: Product = getProduct(store);
   viewRendering(mainContent, product);
   populateProductTypesSelectInput(product);
+  formEvents(store);
 }
 
 //get product
@@ -24,6 +27,14 @@ function getProduct(store: Store): Product {
 //view rendering
 function viewRendering(mainContent: HTMLElement, product: Product): void {
   mainContent.innerHTML = editProductView(product);
+
+  const editForm: HTMLFormElement = document.getElementById(
+    "form"
+  ) as HTMLFormElement;
+
+  editForm["product-name"].value = product.name;
+  editForm["product-image"].value = product.image;
+  editForm["product-thumbnail"].value = product.thumbnail;
 }
 
 //Selector for product types loading
@@ -42,4 +53,54 @@ function populateProductTypesSelectInput(product: Product): void {
 
     typeSelector.appendChild(newOption);
   }
+}
+
+function formEvents(store: Store) {
+  const editForm: HTMLFormElement = document.getElementById(
+    "form"
+  ) as HTMLFormElement;
+
+  const submitButton: HTMLButtonElement = document.getElementById(
+    "edit-product-submit-button"
+  ) as HTMLButtonElement;
+
+  const cancelButton: HTMLButtonElement = document.getElementById(
+    "cancel-edit-product-submit-button"
+  ) as HTMLButtonElement;
+
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    try {
+      const product: Product = getProduct(store);
+      verifyProductEdition(product, editForm);
+      store.editProduct();
+      displayInfoContainer("El producto ha sido editado correctamente.");
+      returnHome();
+    } catch (error) {
+      displayInfoContainer(`${error}`);
+    }
+  });
+
+  cancelButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    displayInfoContainer("El producto no ha sido editado.");
+    returnHome();
+  });
+}
+
+//verify product creation according to Product Class rules
+function verifyProductEdition(
+  product: Product,
+  editProductForm: HTMLFormElement
+): void {
+  product.name = editProductForm["product-name"].value;
+  product.type = editProductForm["product-type"][
+    editProductForm["product-type"].selectedIndex
+  ].text as ProductTypeEnum;
+  product.price = parseFloat(editProductForm["product-price"].value);
+  product.description = editProductForm["product-description"].value;
+  product.image = editProductForm["product-image"].value;
+  product.thumbnail = editProductForm["product-thumbnail"].value;
+  product.stock = parseInt(editProductForm["product-stock"].value);
+  product.discount = parseFloat(editProductForm["product-discount"].value);
 }
