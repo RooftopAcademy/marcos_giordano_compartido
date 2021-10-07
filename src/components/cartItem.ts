bb2804bce1dffb0140059bb3f3f20c457457504f
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-07 11:29:06 -0300

    Create a div element to wrap the content

--- src/components/cartItem.ts
+++ src/components/cartItem.ts
@@ -5,15 +5,16 @@ export default function cartItem(cartItem: CartItem): string {
     <tr>
         <td>${cartItem.item.name}</td>
         <td>${cartItem.item.getPriceWithDiscount().toFixed(0)}</td>
-        <td class="cart-table-item">
-          <button class="cart-button js-substract" data-product-id="${
-            cartItem.item.id
-          }">&minus;</button>
-          ${cartItem.amount}
-          <button class="cart-button js-add" data-product-id="${
-            cartItem.item.id
-          }">&plus;</button>
-        
+        <td>
+          <div class="cart-table-item">
+            <button class="cart-button js-substract" data-product-id="${
+              cartItem.item.id
+            }">&minus;</button>
+            ${cartItem.amount}
+            <button class="cart-button js-add" data-product-id="${
+              cartItem.item.id
+            }">&plus;</button>
+          </div>
         <td>${(cartItem.amount * cartItem.item.getPriceWithDiscount()).toFixed(
           0
         )}</td>
