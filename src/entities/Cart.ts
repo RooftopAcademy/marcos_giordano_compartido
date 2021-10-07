958dcef76436ff5da8f2e568fd1a09c23abbd4da
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-07 11:23:19 -0300

    Set cart as an array of cartitems implementing the interface

--- src/entities/Cart.ts
+++ src/entities/Cart.ts
@@ -1,27 +1,41 @@
+import CartItemInterface from "../interfaces/CartItemInterface";
 import Product from "./Product";
 
 export default class Cart {
-  private _products: Array<Product> = [];
+  private _products: Array<CartItemInterface> = [];
 
   constructor() {}
 
   public add(product: Product): void {
-    this._products.push(product);
+    const itemSearch = this._products.filter(
+      (p: CartItemInterface) => p.item.id == product.id
+    );
+    if (itemSearch.length === 0) {
+      this._products.push({
+        item: product,
+        amount: 1,
+      });
+    } else {
+      itemSearch[0].amount++;
+    }
     localStorage.setItem("cartProducts", JSON.stringify(this._products));
   }
 
-  public showAll(): Array<Product> {
+  public showAll(): Array<CartItemInterface> {
     return this._products;
   }
 
   public load(): void {
     const loadData: string | null = localStorage.getItem("cartProducts");
     if (loadData) {
-      const savedProducts: Array<Product> = JSON.parse(loadData);
+      const savedProducts: Array<CartItemInterface> = JSON.parse(loadData);
       savedProducts.forEach((element) => {
         const product: Product = new Product();
-        product.create(element);
-        this._products.push(product);
+        product.create(element.item);
+        this._products.push({
+          item: product,
+          amount: element.amount,
+        });
       });
     }
   }
