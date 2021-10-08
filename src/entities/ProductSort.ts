40927bb18cf212b84952846d7f5c5adfcec6355c
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-08 19:22:06 -0300

    Create new ProductSort Class

---
+++ src/entities/ProductSort.ts
@@ -0,0 +1,43 @@
+import Order from "../enums/OrderEnum";
+import Product from "./Product";
+
+export default abstract class ProductSort {
+  public result: Array<Product> = [];
+  private sortingMethod: { [index: string]: any } = {};
+  private cache: Map<{ [index: string]: any }, Array<Product>> = new Map();
+
+  public setSorting(sortingMethod: Object = {}) {
+    this.sortingMethod = sortingMethod;
+
+    const sortingMethodKeys: Array<keyof Product> = Object.keys(
+      this.sortingMethod
+    ) as Array<keyof Product>;
+
+    sortingMethodKeys.forEach((sortType) => {
+      this.result = this.SortbyKey(sortType, this.sortingMethod[sortType]);
+      this.cache.set(sortingMethod, this.result);
+    });
+  }
+
+  public getSorting() {
+    return this.cache.get(this.sortingMethod);
+  }
+
+  public SortbyKey(key: keyof Product, order: Order) {
+    if (order === Order.ASC) {
+      return this.result.sort((a, b) => {
+        return this.sortBy(a, b, key);
+      });
+    } else {
+      return this.result.sort((a, b) => {
+        return this.sortBy(b, a, key);
+      });
+    }
+  }
+
+  private sortBy(a: Product, b: Product, key: keyof Product) {
+    if (a[key] > b[key]) return 1;
+    if (a[key] < b[key]) return -1;
+    return 0;
+  }
+}
