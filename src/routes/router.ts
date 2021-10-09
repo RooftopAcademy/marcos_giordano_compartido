d569e2275b8cc9704b448b61c979214e0fb56c4c
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-09 19:17:13 -0300

    Remove product not found from router

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -10,7 +10,6 @@ import { searchViewLogic } from "../viewsLogic/searchViewLogic";
 import { registerViewLogic } from "../viewsLogic/registerViewLogic";
 import { userViewLogic } from "../viewsLogic/userViewLogic";
 import { editProductViewLogic } from "../viewsLogic/editProductViewLogic";
-import { productNotFoundViewLogic } from "../viewsLogic/productNotFoundViewLogic";
 
 const routes: Array<ViewInterface> = [
   { path: "", viewRendering: homeViewLogic },
@@ -22,7 +21,6 @@ const routes: Array<ViewInterface> = [
   { path: "newProduct", viewRendering: newProductViewLogic },
   { path: "editProduct", viewRendering: editProductViewLogic },
   { path: "search", viewRendering: searchViewLogic },
-  { path: "productNotFound", viewRendering: productNotFoundViewLogic },
   { path: "error", viewRendering: errorViewLogic },
 ];
 
