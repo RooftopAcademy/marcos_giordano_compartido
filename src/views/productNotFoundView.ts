f88308e31a994cd7ba7413ea6708e8b128e4f942
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-09 19:16:20 -0300

    Remove product not found view

--- src/views/productNotFoundView.ts
+++
@@ -1,7 +0,0 @@
-export default function productNotFoundView() {
-  return `
-  <div class="product-not-found">
-    <h1>Lo sentimos. No hemos podido encontrar ningún producto con ese criterio de búsqueda.</h1>
-  </div>
-  `;
-}
