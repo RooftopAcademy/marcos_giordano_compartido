b1e9711eb762e515b33ecf5de01fd2bb8781f335
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-09 10:34:16 -0300

    Add search events

--- src/viewsLogic/searchViewLogic.ts
+++ src/viewsLogic/searchViewLogic.ts
@@ -3,8 +3,23 @@ import searchView from "../views/searchView";
 
 export function searchViewLogic(store: Store, mainContent: HTMLElement) {
   viewRendering(mainContent);
+  searchEvent();
 }
 //view rendering
 function viewRendering(mainContent: HTMLElement): void {
   mainContent.innerHTML = searchView();
+}
+
+function searchEvent() {
+  const searchButton: HTMLButtonElement = document.getElementById(
+    "search-view-button"
+  ) as HTMLButtonElement;
+  const searchInput: HTMLInputElement = document.getElementById(
+    "search-view-input"
+  ) as HTMLInputElement;
+
+  searchButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.hash = `#/productList?search=${searchInput.value}`;
+  });
 }
