3f0f853990a263278a585fd2b9d386679e91810a
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-13 16:39:11 -0300

    Upload files to github

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
+app.get("/*", (req, res) => {
+  res.send("Error 404");
+  res.status(404);
+});
+
 app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
