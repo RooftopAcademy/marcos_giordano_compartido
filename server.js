54d8c0d281a5ca8c8a190c1fe9b7db812c6b3942
Marcos Giordano <marcos_l14@hotmail.com>
2021-10-14 15:44:33 -0300

    Add users routes and cors

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const path = require("path");
+const usersRoutes = require("./src-back-end/users.routes");
+const Cors = require("cors");
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(Cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
+
+usersRoutes(app);
 
 app.get("/*", (req, res) => {
   res.send("Error 404");
